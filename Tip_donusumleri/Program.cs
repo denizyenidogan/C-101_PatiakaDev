910c00a315e0608735e7602da7abc3065832f39e
Deniz Yenidogan <deniz.yenidogan@hotmail.com>
2022-07-25 22:39:02 +0300

    Commit

---
+++ Tip_donusumleri/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tip_donusumleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
