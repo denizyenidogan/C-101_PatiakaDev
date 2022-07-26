fc3de82663bf31537f2a0e78fb5d1cf727146dc2
Deniz Yenidogan <deniz.yenidogan@hotmail.com>
2022-07-26 13:31:03 +0300

    commit

--- Hata_Yonetimi/Program.cs
+++ Hata_Yonetimi/Program.cs
@@ -6,11 +6,43 @@ namespace Hata_Yonetimi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bir sayı giriniz:");
-            int sayi = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Girmiş olduğunuz sayı :" + sayi);
-
+            try
+            {
+                Console.WriteLine("Bir sayı giriniz:");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girmiş olduğunuz sayı :" + sayi);    
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hata: "+ ex.Message.ToString());
+                throw;
+            }
             
+            finally
+            {
+                Console.Write("İşlem tamamlandı.");
+            }
+
+            try
+            {
+                int a = int.Parse(null);
+                //int a = int.Parse("test"); //Format exception
+                //int a = int.Parse("-2345678910"); //Overflow exception
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş değer girdiniz");
+                Console.WriteLine(ex);
+                throw;
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Çok küçük yada çok büyük değer girdiniz!");
+                Console.WriteLine(ex);
+            }
+            finally{
+                Console.WriteLine("İşlem başarı ile tamamlandı!");
+            }
         }
     }
 }
