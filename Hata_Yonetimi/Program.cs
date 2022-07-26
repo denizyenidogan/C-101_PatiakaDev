c3f650d725c499447fbef1d2a628431c73c474e0
Deniz Yenidogan <deniz.yenidogan@hotmail.com>
2022-07-26 10:24:10 +0300

    Commit

---
+++ Hata_Yonetimi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hata_Yonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
