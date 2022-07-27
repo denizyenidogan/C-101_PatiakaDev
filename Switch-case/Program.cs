aba017b89d8689c6789ff60b9366e132cf07b4f6
Deniz Yenidogan <deniz.yenidogan@hotmail.com>
2022-07-27 22:00:15 +0300

    Commit

---
+++ Switch-case/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int month = DateTime.Now.Month;
+            Console.WriteLine(month);
+
+            switch (month) // Expresion
+            {
+                case 1:
+                    Console.WriteLine("Ocak ayındasınız!");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat ayındasınız!");
+                    break;
+                case 4:
+                    Console.WriteLine("Nisan ayındasınız!");
+                    break;
+                case 7:
+                    Console.WriteLine("Temmuz ayındasınız!");
+                    break;
+
+                default:
+                    Console.WriteLine("Yanlış veri girdiniz!"); // Veri girişi yapılan yapılarda kullanılması daha anlamlı olacaktır.
+                break;
+            }
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış mesimindesiniz");
+                    break;
+                
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz Mevsimindesiniz");
+                    break;
+
+                default:
+                    Console.WriteLine("Bahar Mevsimindesiniz");
+                break;
+            }
+        }
+    }
+}
