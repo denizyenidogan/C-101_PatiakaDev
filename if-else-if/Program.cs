2f9680bb06bb971036d1d98bd2327d22b6e68b88
Deniz Yenidogan <deniz.yenidogan@hotmail.com>
2022-07-27 21:23:18 +0300

    Commit

--- if-else-if/Program.cs
+++ if-else-if/Program.cs
@@ -6,7 +6,19 @@ namespace if_else_if
     {
         static void Main(string[] args)
         {
-            
+            int time = DateTime.Now.Hour;
+
+            if(time>=6 && time < 11)
+                Console.WriteLine("Good Morning Sir!");
+            else if(time<= 18)
+                Console.WriteLine("Good Day Sir!");
+            else
+                Console.WriteLine("Good Night Sir!");
+
+            string sonuc = time<=18 ? "Good Day Sir!" : "Good Night Sir!";
+
+            sonuc = time>= 6 && time <11 ? "Good Morning Sir!" : time <= 18 ? "Good Day Sir!" : "Good Night Sir!";
+            Console.WriteLine(sonuc);
         }
     }
 }
