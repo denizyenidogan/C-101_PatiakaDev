1a2a515a7aa264eefea6361cb90a6e7127a35cfe
Deniz Yenidogan <deniz.yenidogan@hotmail.com>
2022-07-29 23:00:26 +0300

    Commit

--- Donguler-while-foreach/Program.cs
+++ Donguler-while-foreach/Program.cs
@@ -6,7 +6,36 @@ namespace Donguler_while_foreach
     {
         static void Main(string[] args)
         {
-            
+            //While
+            // 1 den başlayarak console dan girilen sayıya kadar(sayı dahil) ortalama hesaplayıp, console a yazdıran program.
+            Console.WriteLine("Lütfen bir sayi giriniz: ");
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac = 1;
+            int toplam = 0;
+            while (sayac<= sayi)
+            {
+                toplam += sayac;
+                sayac ++;
+            }
+
+            Console.WriteLine(toplam/sayi);
+
+            //'a' dan 'z' ye kadar tüm harfleri console a yazar.
+            char character = 'a';
+            while(character < 'z')
+            {
+                Console.Write(character);
+                character ++;
+            }
+
+
+            Console.WriteLine("*****Foreach*****");
+            string[] arabalar = {"bmw","Ford","Toyota","Nissan"};
+
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
         }
     }
 }
