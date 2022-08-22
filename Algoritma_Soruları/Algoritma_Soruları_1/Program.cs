570eda466993e12d965f2616f76f5abd44f4a6ef
denizyenidogan <109694283+denizyenidogan@users.noreply.github.com>
2022-08-22 22:14:03 +0300

    Add files via upload

---
+++ Algoritma_Soruları/Algoritma_Soruları_1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Algoritma_Soruları_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n).Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. Kullanıcının girmiş olduğu sayılardan çift olanlar console'a yazdırın.
+            
+            Console.WriteLine("Lütfen Dizinin eleman sayısını giriniz : ");
+            int diziUzunlugu = int.Parse(Console.ReadLine());
+            int[] sayıDizisi = new int [diziUzunlugu];
+
+            for (int i = 0; i < diziUzunlugu; i++)
+            {
+                Console.Write("Lütfen {0}. sayıyı giriniz: ", i+1);
+                sayıDizisi[i] = int.Parse(Console.ReadLine());
+            }
+
+            foreach (var sayi in sayıDizisi)
+            {
+                if (sayi % 2 == 0)
+                    Console.WriteLine(sayi);
+            }
+        }
+    }
+}
